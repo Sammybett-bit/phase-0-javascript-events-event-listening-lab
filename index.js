d320e4a1b30873435ef883ce60a2742330ef4703
Sammybett-bit <bettsammy20@gmail.com>
2023-07-18 23:47:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// let input;
+
+// function addingEventListener() {
+//     input = document.getElementById('input');
+
+// }
+let input;
+
 function addingEventListener() {
+  input = document.getElementById('input');
+  
+  input.addEventListener('change', inputChange);
+}
+
+function inputChange(event) {
+  console.log('Input value changed:', event.target.value);
 }
